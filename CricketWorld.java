7e1ebb4aa0d9f1c610a5452dfdb3d8ba9e5d5000
Prashantb07 <prashantb.xworkz@gmail.com>
2022-11-23 20:42:51 +0530

    Committing Java files

---
+++ CricketWorld.java
@@ -0,0 +1,28 @@
+class CricketWorld{
+
+    static  String topBowlersInOdi[] = {"Trent Boult", "Josh Hazlewood", "Mujeeb Ur Rahman", "Shaheen Afridi", "Mohammad Nabi"};
+	static  String top5WicketTakersInOdiMen[] = {"M Muralidaran", "Wasim Akram", "Waqar Younis", "Shahid Afridi", "SM Pollock"};
+	static  String teamsWinningWorldCup[] = {"Australia", "West Indies", "India", "Pakistan", "Srilanka", "England"};
+	static  int    noOfTimesTeamWinningWorldCup[] = {5,2,2,1,1,1};
+	
+	   public static void main(String cricket[]){
+	   
+	System.out.println("Mens Top 5 Bowlers In ODI");
+	System.out.println(topBowlersInOdi[0] + "   "+ topBowlersInOdi[1] + "   "+ topBowlersInOdi[2] + "   "+ topBowlersInOdi[3] + "   "+ topBowlersInOdi[4]);
+	System.out.println("Top 5 Wicket takers in ODI cricket");
+	System.out.println(top5WicketTakersInOdiMen[0] + "   "+ top5WicketTakersInOdiMen[1] + "   "+ top5WicketTakersInOdiMen[2] + "   "+ top5WicketTakersInOdiMen[3] + "   "+ top5WicketTakersInOdiMen[4]);
+	System.out.println("Teams Winning The World Cup are:");
+	System.out.println(teamsWinningWorldCup[0]+ "   "+ teamsWinningWorldCup[1]+ "   " + teamsWinningWorldCup[2] + "   "+ teamsWinningWorldCup[3] + "   "+ teamsWinningWorldCup[4] + "    "+ teamsWinningWorldCup[5]);
+	System.out.println("Number of times the teams winning the World Cup");
+	System.out.println(noOfTimesTeamWinningWorldCup[0]+ "   "+ noOfTimesTeamWinningWorldCup[1] + "   "+ noOfTimesTeamWinningWorldCup[2]+ "    "+ noOfTimesTeamWinningWorldCup[3]+ "   "+ noOfTimesTeamWinningWorldCup[4] + "    "+ noOfTimesTeamWinningWorldCup[5]);
+	
+	   
+	   }
+
+
+
+
+
+
+
+}
---
+++ Customer.java
@@ -0,0 +1,18 @@
+class Customer {
+
+ public static void main(String a[]){
+ 
+      String  userName = "uma" ;
+	  int      usersAge  =  52 ;
+	  long     usersContactNo = 8861174016L ;
+	  boolean   isAlive  =  true ;
+	  System.out.println("the user's Information are");
+	  System.out.println("The user's name is "+ userName);
+	  System.out.println("The user's Age is " + usersAge) ;
+	  System.out.println("The user's Contact number is " + usersContactNo) ;
+	  System.out.println("Is the user Alive " + isAlive) ;
+ }
+
+
+
+}
