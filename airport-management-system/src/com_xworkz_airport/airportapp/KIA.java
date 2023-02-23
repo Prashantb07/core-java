cc698d0c8f06470a948d6e1045ad995802654913
Prashantb07 <118912007+Prashantb07@users.noreply.github.com>
2023-02-23 18:53:11 +0530

    Add files via upload

---
+++ airport-management-system/src/com_xworkz_airport/airportapp/KIA.java
@@ -0,0 +1,11 @@
+package com_xworkz_airport.airportapp;
+
+import com_xworkz_airport.airportapp.airport.Airport;
+
+public class KIA extends Airport{
+
+	@Override
+	public void getdisplay() {
+		System.out.println("Display");
+	}
+}
---
+++ airport-management-system/src/com_xworkz_airport/airportapp/TerminalTester.java
@@ -0,0 +1,17 @@
+package com_xworkz_airport.airportapp;
+
+import com_xworkz_airport.airportapp.terminal.Terminal;
+
+public class TerminalTester {
+
+	public static void main(String[] args) {
+		//Airport airport =new Airport();
+		KIA kia = new KIA();
+		Terminal terminal =new Terminal("ter 1","xyz","bagalkot",5);
+		kia.trip(terminal);
+		kia.getdisplay();
+	}
+	
+	
+	
+} 
